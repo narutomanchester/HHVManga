80bb0a278227f8d5a4be7ec3b7e400bc127f666f
Nguyen Le Vu Long <ssgreendragon@gmail.com>
2017-03-11 00:54:02 +0700

    Add search API

---
+++ VManga/VManga/Debug.swift
@@ -0,0 +1,9 @@
+//
+//  Debug.swift
+//  VManga
+//
+//  Created by Nguyen Le Vu Long on 3/10/17.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+import Foundation
---
+++ VManga/VManga/HH.swift
@@ -0,0 +1,21 @@
+//
+//  HH.swift
+//  VManga
+//
+//  Created by Nguyen Le Vu Long on 3/10/17.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+import UIKit
+
+class HH: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
