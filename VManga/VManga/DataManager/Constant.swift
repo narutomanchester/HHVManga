49ba0a5f19280c0ba5df841648ac046e322de77e
HiepDz <narutomanchester@gmail.com>
2017-03-10 19:35:08 +0700

    up code git ios

---
+++ VManga/VManga/DataManager/Constant.swift
@@ -0,0 +1,9 @@
+//
+//  Constant.swift
+//  VManga
+//
+//  Created by mac on 3/10/17.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+let urlList100books = "http://wannashare.info/api/v1/list?$limit=100&$skip=0"
---
+++ VManga/VManga/DataManager/SearchManager.swift
@@ -0,0 +1,9 @@
+//
+//  SearchManager.swift
+//  VManga
+//
+//  Created by mac on 3/10/17.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+import Foundation
